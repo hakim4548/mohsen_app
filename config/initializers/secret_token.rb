eeb2896cf00f77ce64a5af33f7ab710d3dc60c0f
mohsen <hakim13714548@gmail.com>
2013-08-21 10:05:26 +0430

    mohsen first application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MohsenApp::Application.config.secret_key_base = '2bf883949d67b47cc4f923bc9ccc795af5c428dc2d3a518bc426d57d4e996b10b56c7376e6d32dca19ebf20023f6ce67553c705bf63f681ac1d44e08a6c31c62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MohsenApp::Application.config.session_store :cookie_store, key: '_mohsen-app_session'
